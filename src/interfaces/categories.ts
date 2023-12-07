e3317f784a82a4b36e7d855a3b16d0c664b2842c
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-12-07 18:58:04 +0200

    feat(): Added new interfaces

---
+++ src/interfaces/categories.ts
@@ -0,0 +1,5 @@
+export default interface ICategory {
+  id: number;
+  name: string;
+  image: string;
+}
---
+++ src/interfaces/subcategories.ts
@@ -0,0 +1,5 @@
+export default interface ISubcategories {
+  id: number;
+  name: string;
+  image: string;
+}
