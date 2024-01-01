0fb0f239fd5ec505b0ca74126996fa9fce07dfde
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2024-01-01 15:42:07 +0200

    added edit and delete actions in table component without database interaction

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,40 +1,167 @@
-import s from "./Table.module.scss";
-
-import * as React from "react";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
+import {
+  GridRowsProp,
+  GridRowModesModel,
+  GridRowModes,
+  DataGrid,
+  GridColDef,
+  GridActionsCellItem,
+  GridEventListener,
+  GridRowId,
+  GridRowModel,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
 
 interface Column {
   field: string;
   headerName: string;
+  width?: number;
+  editable?: boolean;
   type?: string;
-  width: number;
-}
-
-interface Row {
-  [key: string]: number | string;
+  align?: "left" | "center" | "right";
+  headerAlign?: "left" | "center" | "right";
+  valueOptions?: string[];
 }
 
 interface DataTableProps {
   columns: Column[];
-  rows: Row[];
+  rows: GridRowsProp;
 }
 
-const DataTable: React.FC<DataTableProps> = ({ columns, rows }) => {
+export const FullFeaturedCrudGrid: React.FC<DataTableProps> = ({
+  columns,
+  rows,
+}) => {
+  const [gridRows, setGridRows] = useState(rows);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setGridRows(gridRows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = gridRows.find((row) => row.id === id);
+    if (editedRow!.isNew) {
+      setGridRows(gridRows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setGridRows(
+      gridRows.map((row) => (row.id === newRow.id ? updatedRow : row))
+    );
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const actionsColumn: GridColDef = {
+    field: "actions",
+    type: "actions",
+    headerName: "Actions",
+    width: 100,
+    cellClassName: "actions",
+    getActions: ({ id }) => {
+      const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+      if (isInEditMode) {
+        return [
+          <GridActionsCellItem
+            icon={<SaveIcon />}
+            label="Save"
+            sx={{
+              color: "primary.main",
+            }}
+            onClick={handleSaveClick(id)}
+          />,
+          <GridActionsCellItem
+            icon={<CancelIcon />}
+            label="Cancel"
+            className="textPrimary"
+            onClick={handleCancelClick(id)}
+            color="inherit"
+          />,
+        ];
+      }
+
+      return [
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          label="Edit"
+          className="textPrimary"
+          onClick={handleEditClick(id)}
+          color="inherit"
+        />,
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          label="Delete"
+          onClick={handleDeleteClick(id)}
+          color="inherit"
+        />,
+      ];
+    },
+  };
+
+  const tableColumns: GridColDef[] = [...columns, actionsColumn];
+
   return (
-    <div className={s.table}>
+    <Box
+      sx={{
+        height: 500,
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
+        },
+        "& .textPrimary": {
+          color: "text.primary",
+        },
+        "& .MuiDataGrid-columnHeaders": {
+          backgroundColor: "#F8F0FB",
+        },
+      }}
+    >
       <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 10 },
-          },
+        rows={gridRows}
+        columns={tableColumns}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        slotProps={{
+          toolbar: { setRows: setGridRows, setRowModesModel },
         }}
-        pageSizeOptions={[10, 25, 50, 100]}
-        checkboxSelection
       />
-    </div>
+    </Box>
   );
 };
-
-export default DataTable;
