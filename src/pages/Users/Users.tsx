36c9db7901f0425a95eefc041b8f94344cf3206d
rryaboshtan <rrvworkroman@gmail.com>
2024-01-03 00:18:04 +0200

    fix users table

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,43 +1,42 @@
-import { useEffect, useState } from "react";
-import s from "./Users.module.scss";
+import { useEffect, useState } from 'react'
+import s from './Users.module.scss'
 
-import Select from "src/components/ToolsPanel/Select/Select";
-import SearchInput from "src/components/ToolsPanel/SearchInput/SearchInput";
-import FilterIcon from "../../images/svg/FilterIcon";
-import ActionsIcon from "../../images/svg/ActionsIcon";
-import ExportFileIcon from "../../images/svg/ExportFileIcon";
-import RowListIcon from "../../images/svg/RowListIcon";
-import axios from "axios";
-import AdminLayout from "src/layouts/AdminLayout";
-import { FullFeaturedCrudGrid } from "src/components/Table/Table";
-import { handleExternalDataUpdate } from "../../components/Table/helpers";
+import Select from 'src/components/ToolsPanel/Select/Select'
+import SearchInput from 'src/components/ToolsPanel/SearchInput/SearchInput'
+import FilterIcon from '../../images/svg/FilterIcon'
+import ActionsIcon from '../../images/svg/ActionsIcon'
+import ExportFileIcon from '../../images/svg/ExportFileIcon'
+import RowListIcon from '../../images/svg/RowListIcon'
+import axios from 'axios'
+import AdminLayout from 'src/layouts/AdminLayout'
+import { FullFeaturedCrudGrid } from 'src/components/Table/Table'
+import { handleExternalDataUpdate } from '../../components/Table/helpers'
 
 export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  note: string;
-  phone: string;
-  createdAt: string;
-  updatedAt: string;
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+  note: string
+  phone: string
+  createdAt: string
+  updatedAt: string
 }
 interface TableUser {
-  id: number;
-  // fullName: string;
-  // email: string;
-  note: string;
-  // phone: string;
-  // createdAt: string;
+  id: number
+  fullName: string
+  email: string
+  note: string
+  phone: string
+  createdAt: string
 }
 
 const Users = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [mapedUsers, setMapedUsers] = useState<TableUser[]>([]);
+  const [users, setUsers] = useState<TableUser[]>([])
 
   useEffect(() => {
     const getUsers = async () => {
-      const apiUrl = "http://16.171.113.245:5000/api/user?page=1";
+      const apiUrl = 'http://16.171.113.245:5000/api/user?page=1'
       // "http://16.171.113.245:5000/api/user?page=1&lookup=323&pageSize=25";
 
       try {
@@ -45,30 +44,39 @@ const Users = () => {
           headers: {
             Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsImVtYWlsIjoiYWRtaXdlMjM0MzI0MmZ3ZW5Ad2Vmd2UyMy5jb20iLCJpc0FkbWluIjoidHJ1ZSIsImlhdCI6MTcwMzU2OTU4NCwiZXhwIjoxNzExMzQ1NTg0fQ.ZCj9Ub0jTLqCOtKTDI1CA-8hDDsLGOcp1-0qgVXMDr8`,
           },
-        });
+        })
 
-        const usersData = response.data.rows;
-        // response.data.rows[0]
+        const usersData: User[] = response.data.rows
 
-        setUsers(usersData);
-
-        // console.log("response.data: ", usersData);
+        setUsers(
+          usersData.map(user => ({
+            id: user.id,
+            fullName: `${user.firstName} ${user.lastName}`,
+            email: user.email,
+            phone: user.phone,
+            note: user.note,
+            createdAt: user.createdAt,
+          }))
+        )
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error)
       }
-    };
+    }
 
-    getUsers();
-  }, []);
+    getUsers()
+  }, [])
 
-  useEffect(() => {
-    const tableRows: TableUser[] = users.map((user) => ({
-      id: user.id,
-      note: user.note,
-    }));
-    setMapedUsers(tableRows);
-    console.log(tableRows);
-  }, [users]);
+  // useEffect(() => {
+  //   const tableRows: TableUser[] = users.map(user => ({
+  //     id: user.id,
+  //     fullName: `${user.firstName} ${user.lastName}`,
+  //     email: user.email,
+  //     phone: user.phone,
+  //     note: user.note,
+  //     createdAt: user.createdAt,
+  //   }))
+  //   setMapedUsers(tableRows)
+  // }, [users])
 
   // console.log("tableRows: ", tableRows);
 
@@ -81,70 +89,54 @@ const Users = () => {
   //   createdAt: "effefefef",
   // }));
 
-  const filteringOptions = ["Option 1", "Option 2", "Option 3"];
-  const actionOptions = ["Редагувати", "Видалити"];
-  const exportOptions = ["Option 1", "Option 2", "Option 3"];
-  const paginationOptions = ["Option 1", "Option 2", "Option 3"];
+  const filteringOptions = ['Option 1', 'Option 2', 'Option 3']
+  const actionOptions = ['Редагувати', 'Видалити']
+  const exportOptions = ['Option 1', 'Option 2', 'Option 3']
+  const paginationOptions = ['Option 1', 'Option 2', 'Option 3']
 
   const customColumns = [
-    // {
-    //   field: "createdAt",
-    //   headerName: "Додано",
-    //   type: "string",
-    //   width: 130,
-    //   editable: true,
-    // },
-
     {
-      field: "id",
-      headerName: "ID",
+      field: 'id',
+      headerName: 'ID',
       width: 70,
-      type: "number",
+      type: 'number',
     },
-    // {
-    //   field: "fullName",
-    //   headerName: "Ім’я",
-    //   width: 130,
-    //   type: "string",
-    //   editable: true,
-    // },
-    // {
-    //   field: "firstName",
-    //   headerName: "Ім’я",
-    //   width: 130,
-    //   type: "string",
-    //   editable: true,
-    // },
-    // {
-    //   field: "lastName",
-    //   headerName: "Ім’я",
-    //   width: 130,
-    //   type: "string",
-    //   editable: true,
-    // },
     {
-      field: "note",
-      headerName: "Примітки",
-      type: "string",
+      field: 'fullName',
+      headerName: 'Ім’я',
+      width: 130,
+      type: 'string',
+      editable: true,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 130,
+      type: 'string',
+      editable: true,
+    },
+    {
+      field: 'phone',
+      headerName: 'Телефон',
+      type: 'number',
       width: 130,
       editable: true,
     },
-    // {
-    //   field: "email",
-    //   headerName: "Email",
-    //   width: 130,
-    //   type: "string",
-    //   editable: true,
-    // },
-
-    // {
-    //   field: "phone",
-    //   headerName: "Телефон",
-    //   type: "number",
-    //   width: 130,
-    //   editable: true,
-    // },
-  ];
+    {
+      field: 'note',
+      headerName: 'Примітки',
+      type: 'string',
+      width: 130,
+      editable: true,
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Додано',
+      type: 'string',
+      width: 130,
+      editable: true,
+    },
+  ]
 
   // const tableRows = [
   //   {
@@ -185,40 +177,25 @@ const Users = () => {
               <SearchInput />
               <div className={s.tools}>
                 <section className={s.options__container}>
-                  <Select
-                    options={filteringOptions}
-                    icon={<FilterIcon />}
-                    toolName={"Фільтрувати"}
-                  />
-                  <Select
-                    options={actionOptions}
-                    icon={<ActionsIcon />}
-                    toolName={"Дії"}
-                  />
-                  <Select
-                    options={exportOptions}
-                    icon={<ExportFileIcon />}
-                    toolName={"Експортувати"}
-                  />
+                  <Select options={filteringOptions} icon={<FilterIcon />} toolName={'Фільтрувати'} />
+                  <Select options={actionOptions} icon={<ActionsIcon />} toolName={'Дії'} />
+                  <Select options={exportOptions} icon={<ExportFileIcon />} toolName={'Експортувати'} />
                 </section>
                 <Select
                   options={paginationOptions}
                   icon={<RowListIcon />}
-                  toolName={"Рядків на сторінці: 10"}
-                  style={{ width: "261px" }}
+                  toolName={'Рядків на сторінці: 10'}
+                  style={{ width: '261px' }}
                 />
               </div>
             </div>
           </div>
-          <FullFeaturedCrudGrid
-            columns={customColumns}
-            rows={users}
-            onExternalDataUpdate={handleExternalDataUpdate}
-          />
+
+          <FullFeaturedCrudGrid columns={customColumns} rows={users} onExternalDataUpdate={handleExternalDataUpdate} />
         </section>
       </main>
     </AdminLayout>
-  );
-};
+  )
+}
 
-export default Users;
+export default Users
