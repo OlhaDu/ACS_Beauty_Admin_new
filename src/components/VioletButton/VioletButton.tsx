3efbea217bab36048184de73886a8dec58530fb6
Konstantin Ligus <ligus.konstantin@gmail.com>
2024-01-03 16:09:38 +0200

    change VioletButton, create Border

--- src/components/VioletButton/VioletButton.tsx
+++ src/components/VioletButton/VioletButton.tsx
@@ -2,8 +2,10 @@ import { FC } from "react";
 import s from "./VioletButton.module.scss";
 import { IVioletButtonProps } from "src/types";
 
-const Button: FC<IVioletButtonProps> = ({ title }) => (
-  <button className={s.button}>{title}</button>
+const Button: FC<IVioletButtonProps> = ({ title, onClick }) => (
+  <button className={s.button} onClick={onClick}>
+    {title}
+  </button>
 );
 
 export default Button;
