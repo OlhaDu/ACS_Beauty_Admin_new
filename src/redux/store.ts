0c62348f7240c24a68c68f49cc36e2d7de3d126d
rryaboshtan <rrvworkroman@gmail.com>
2024-01-22 22:01:53 +0200

    fix reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,8 +2,9 @@ import { configureStore } from "@reduxjs/toolkit"
 import categories from "./slices/categoriesSlice"
 import { TypedUseSelectorHook, useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
+import { brandsReducer } from "./brands/slice"
 
-export const store = configureStore({ reducer: { categories } })
+export const store = configureStore({ reducer: { categories, brands: brandsReducer } })
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
