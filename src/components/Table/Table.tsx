6b55ef0a2b6cfc7af10fd169fd36e6096bdca2b2
iryna-zatynina <ira.zat1997@gmail.com>
2024-01-22 18:09:53 +0100

    resolved remarks

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,60 +1,57 @@
 import s from "./Table.module.scss";
 
 import * as React from "react";
-import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import DeleteIcon from "../../images/svg/DeleteIcon.tsx";
 import EditIcon from "../../images/svg/EditIcon.tsx";
-import {OrdersColumn, OrdersRow} from "../../types/IOrders.ts";
-
+import { OrdersColumn, OrdersRow } from "../../types/IOrders.ts";
 
 interface Props {
-    columns: OrdersColumn[];
-    rows: OrdersRow[];
-    onEdit: (id: number) => void;
-    onDelete: (id: number) => void;
+  columns: OrdersColumn[];
+  rows: OrdersRow[];
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
 }
 
-const Table: React.FC<Props> = ({columns, rows, onEdit, onDelete}) => {
-
-
-    const columnsWithActions: GridColDef[] = [
-        ...columns,
-        {
-            field: 'actions',
-            headerName: 'ДіЇ',
-            width: 100,
-            renderCell: (params) => (
-                <div className={s.actionButtons}>
-                    <button onClick={() => onEdit(params.row.id as number)}>
-                        <EditIcon className={s.svg}/>
-                    </button>
-                    <button onClick={() => onDelete(params.row.id as number)}>
-                        <DeleteIcon className={s.svg}/>
-                    </button>
-                </div>
-            ),
-            headerClassName: s.headerCell,
-        },
-    ];
-
-    columnsWithActions.forEach((column) => {
-        column.headerClassName = s.headerCell;
-    });
-
-    return (
-        <div className={s.table}>
-            <DataGrid
-                rows={rows}
-                columns={columnsWithActions}
-                initialState={{
-                    pagination: {
-                        paginationModel: {page: 0, pageSize: 10},
-                    },
-                }}
-                pageSizeOptions={[10, 25, 50, 100]}
-            />
+const Table: React.FC<Props> = ({ columns, rows, onEdit, onDelete }) => {
+  const columnsWithActions: GridColDef[] = [
+    ...columns,
+    {
+      field: "actions",
+      headerName: "ДіЇ",
+      width: 100,
+      renderCell: (params) => (
+        <div className={s.actionButtons}>
+          <button onClick={() => onEdit(params.row.id as number)}>
+            <EditIcon className={s.svg} />
+          </button>
+          <button onClick={() => onDelete(params.row.id as number)}>
+            <DeleteIcon className={s.svg} />
+          </button>
         </div>
-    );
+      ),
+      headerClassName: s.headerCell,
+    },
+  ];
+
+  columnsWithActions.forEach((column) => {
+    column.headerClassName = s.headerCell;
+  });
+
+  return (
+    <div className={s.table}>
+      <DataGrid
+        rows={rows}
+        columns={columnsWithActions}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 10 },
+          },
+        }}
+        pageSizeOptions={[10, 25, 50, 100]}
+      />
+    </div>
+  );
 };
 
 export default Table;
--- src/components/ToolsPanel/Select/Select.tsx
+++ src/components/ToolsPanel/Select/Select.tsx
@@ -1,53 +1,53 @@
-import React, {useState} from "react";
-import styles from "./Select.module.scss";
+import React, { useState } from "react";
+import s from "./Select.module.scss";
 // import DropdownArrow from "../../../svgs/DropdownArrow";
 import DropdownArrow from "src/images/svg/DropdownArrow";
 
 interface Props {
-    options: string[];
-    toolName: string;
-    icon: React.ReactElement;
-    style?: React.CSSProperties;
+  options: string[];
+  toolName: string;
+  icon: React.ReactElement;
+  style?: React.CSSProperties;
 }
 
-const Select: React.FC<Props> = ({options, toolName, icon, style}) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedValue, setSelectedValue] = useState("");
+const Select: React.FC<Props> = ({ options, toolName, icon, style }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("");
 
-    const handleToggle = () => {
-        setIsOpen(!isOpen);
-    };
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleOptionClick = (option: string) => {
-        setSelectedValue(option);
-        setIsOpen(false);
-    };
+  const handleOptionClick = (option: string) => {
+    setSelectedValue(option);
+    setIsOpen(false);
+  };
 
-    return (
-        <div className={styles.container} onClick={handleToggle} style={style}>
-            <div className={styles.fieldContainer}>
-                <div className={styles.icon}>{icon}</div>
-                <div className={styles.dropdownArrow}>
-                    <DropdownArrow/>
-                </div>
-                {selectedValue || toolName}
-            </div>
-            <div
-                className={styles.optionListContainer}
-                style={!isOpen ? {borderTop: "none"} : {}}
-            >
-                {isOpen && (
-                    <ul>
-                        {options.map((option, index) => (
-                            <li key={index} onClick={() => handleOptionClick(option)}>
-                                {option}
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+  return (
+    <div className={s.container} onClick={handleToggle} style={style}>
+      <div className={s.fieldContainer}>
+        <div className={s.icon}>{icon}</div>
+        <div className={s.dropdownArrow}>
+          <DropdownArrow />
         </div>
-    );
+        {selectedValue || toolName}
+      </div>
+      <div
+        className={s.optionListContainer}
+        style={isOpen ? {} : { borderTop: "none" }}
+      >
+        {isOpen && (
+          <ul>
+            {options.map((option, index) => (
+              <li key={index} onClick={() => handleOptionClick(option)}>
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Select;
