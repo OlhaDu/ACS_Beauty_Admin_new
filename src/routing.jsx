ffb36cc6f623740453d3dd9f6d4de108b4930406
andrii-pozniak <andriipozniak@gmail.com>
2024-01-24 21:34:59 +0200

    add change in package.json

--- src/routing.jsx
+++ src/routing.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import { createBrowserRouter } from "react-router-dom";
-import Users from "./pages/Users/Users";
-import Categories from "./pages/Categories/Categories";
-// import Dashboard from "./pages/Dashboard/Dashboard";
 import ReviewsList from "src/components/Reviews/ReviewsList/ReviewsList";
 import Content from "./components/Reviews/PaginationItem/PaginationItem";
-import AdminLayout from "./layouts/AdminLayout"
-=======
 import { createBrowserRouter } from "react-router-dom"
 import Users from "./pages/Users/Users"
 import CategoriesPage from "./pages/CategoriesPage"
 import Dashboard from "./pages/Dashboard/Dashboard"
 import Brands from "./pages/Brands/Brands"
 import Orders from "./pages/Orders/Orders.tsx"
->>>>>>> 8b1bf786444514c314348d81194ef291537e2539
 
 export const router = createBrowserRouter([
   {
@@ -29,14 +21,10 @@ export const router = createBrowserRouter([
   },
   { path: 'reviews/:page', element: <Content /> },
   {
-<<<<<<< HEAD
-    path: '/',
-    element: <AdminLayout />,
-=======
     path: "/",
     element: <Dashboard />,
->>>>>>> 8b1bf786444514c314348d81194ef291537e2539
   },
+  
   {
     path: "/brands",
     element: <Brands />,
