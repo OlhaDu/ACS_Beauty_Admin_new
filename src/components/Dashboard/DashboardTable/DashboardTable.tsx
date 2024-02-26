7719c501869c669d68da8998a34e9b5635acb2c1
andrii-pozniak <andriipozniak@gmail.com>
2024-02-26 21:02:18 +0200

    add fix in dashboard

--- src/components/Dashboard/DashboardTable/DashboardTable.tsx
+++ src/components/Dashboard/DashboardTable/DashboardTable.tsx
@@ -1,108 +1,105 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useAppDispatch } from "src/redux/store";
-import { selectDashboards } from "src/redux/dashboards/selectors";
+import React from "react"
+import { useSelector } from "react-redux"
+import { useAppDispatch } from "src/redux/store"
+import { selectDashboards } from "src/redux/dashboards/selectors"
 import { DataGrid, GridColDef, GridActionsCellItem } from "@mui/x-data-grid"
 import { columns } from "./columns"
 import DeleteIcon from "src/images/svg/DeleteIconTS"
 import Box from "@mui/material/Box"
 import s from "./DashboardTable.module.scss"
 import EditIcon from "src/images/svg/EditIcon"
-import { deleteDashboard } from "src/redux/dashboards/operations";
+import { deleteDashboard } from "src/redux/dashboards/operations"
 
 interface IProps {
-    page: number
-    pageSize: number
-    setPage: (page: number) => void
-    setPageSize: (pageSize: number) => void
-  }
-  
-  const DashboardTable: React.FC<IProps> = ({ page, pageSize, setPage, setPageSize }) => {
-    const dispatch = useAppDispatch()
-    const dashboards = useSelector(selectDashboards);    // const count = useSelector(selectCount)
-     
-    const actionsColumn: GridColDef = {
-        field: "actions",
-        type: "actions",
-        headerName: "Дії",
-        width: 140,
-        cellClassName: "actions",
-    
-        getActions: ({ id }) => {
-          return [
-            <GridActionsCellItem
-              icon={<EditIcon />}
-              label="Edit"
-              className="textPrimary"
-              color="inherit"
-              onClick={() => {
-            
-              }}
-            />,
-            <GridActionsCellItem
-            icon={<DeleteIcon fill={"black"} width={53} height={53} />}
-              label="Delete"
-              color="inherit"
-              onClick={() => {
-                dispatch(deleteDashboard(id))
-              }}
-            />,
-          ]
-        },
-      }
-    
-      const tableColumns: GridColDef[] = columns.map(col =>
-        col.field === "description"
-          ? {
-              ...col,
-              renderCell: ({ value }) => <div style={{ whiteSpace: "normal" }}>{value}</div>,
-            }
-          : col
-      )
-    
-      tableColumns.push(actionsColumn)
-    
-    return (
-      <>
-        <Box
-          className={s.container}
-          sx={{
-            ".MuiDataGrid-columnHeaders": {
-              backgroundColor: "#F8F0FB",
-              color: " #5c5e60",
-              fontSize: "16px",
-            },
-            ".MuiDataGrid-cell": {
-              color: " #5c5e60",
-              fontSize: "16px",
-            },
-            ".MuiDataGrid-cell:focus": {
-              outline: "none !important",
-            },           
+  page: number
+  pageSize: number
+  setPage: (page: number) => void
+  setPageSize: (pageSize: number) => void
+}
+
+const DashboardTable: React.FC<IProps> = ({ page, pageSize, setPage, setPageSize }) => {
+  const dispatch = useAppDispatch()
+  const dashboards = useSelector(selectDashboards) // const count = useSelector(selectCount)
+
+  const actionsColumn: GridColDef = {
+    field: "actions",
+    type: "actions",
+    headerName: "Дії",
+    width: 140,
+    cellClassName: "actions",
+
+    getActions: ({ id }) => {
+      return [
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          label="Edit"
+          className="textPrimary"
+          color="inherit"
+          onClick={() => {}}
+        />,
+        <GridActionsCellItem
+          icon={<DeleteIcon fill={"black"} width={53} height={53} />}
+          label="Delete"
+          color="inherit"
+          onClick={() => {
+            dispatch(deleteDashboard(id))
           }}
-        >
-          <DataGrid
-            rows={dashboards}
-            columns={tableColumns}
-            checkboxSelection={false}
-            disableRowSelectionOnClick={true}
-            isCellEditable={() => false}
-            isRowSelectable={() => false}
-            // rowCount={count}
-            paginationMode="server"
-            paginationModel={{ page, pageSize }}
-            onPaginationModelChange={({ page, pageSize }) => {
-              setPage(page)
-              setPageSize(pageSize)
-            }}
-            pageSizeOptions={[10, 25, 50, 100]}
-            columnHeaderHeight={44}
-            rowHeight={107}
-          />
-        </Box>
-      </>
-    )
+        />,
+      ]
+    },
   }
-  
-  export default DashboardTable
-  
+
+  const tableColumns: GridColDef[] = columns.map(col =>
+    col.field === "description"
+      ? {
+          ...col,
+          renderCell: ({ value }) => <div style={{ whiteSpace: "normal" }}>{value}</div>,
+        }
+      : col
+  )
+
+  tableColumns.push(actionsColumn)
+
+  return (
+    <>
+      <Box
+        className={s.container}
+        sx={{
+          ".MuiDataGrid-columnHeaders": {
+            backgroundColor: "#F8F0FB",
+            color: " #5c5e60",
+            fontSize: "16px",
+          },
+          ".MuiDataGrid-cell": {
+            color: " #5c5e60",
+            fontSize: "16px",
+          },
+          ".MuiDataGrid-cell:focus": {
+            outline: "none !important",
+          },
+        }}
+      >
+        <DataGrid
+          rows={dashboards}
+          columns={tableColumns}
+          checkboxSelection={false}
+          disableRowSelectionOnClick={true}
+          isCellEditable={() => false}
+          isRowSelectable={() => false}
+          // rowCount={count}
+          paginationMode="server"
+          paginationModel={{ page, pageSize }}
+          onPaginationModelChange={({ page, pageSize }) => {
+            setPage(page)
+            setPageSize(pageSize)
+          }}
+          pageSizeOptions={[10, 25, 50, 100]}
+          columnHeaderHeight={44}
+          rowHeight={107}
+        />
+      </Box>
+    </>
+  )
+}
+
+export default DashboardTable
--- src/components/Dashboard/DashboardTable/columns.tsx
+++ src/components/Dashboard/DashboardTable/columns.tsx
@@ -3,103 +3,89 @@ import { GridColDef } from "@mui/x-data-grid"
 import s from "./DashboardTable.module.scss"
 
 const statusColors: { [key: string]: string } = {
-  'Hoве': s.new_status,
-  'Прийнято': s.accepted_status,
-  'Оплачено': s.paid_status,
-  'Виконано': s.paid_status,
-  'Скасовано': s.cancelled_status,
-};
+  Hoве: s.new_status,
+  Прийнято: s.accepted_status,
+  Оплачено: s.paid_status,
+  Виконано: s.paid_status,
+  Скасовано: s.cancelled_status,
+}
 
 export const columns: GridColDef[] = [
-    {
-      field: "id",
-      headerName: "№",
-      width: 90,
-      align: "center",
-      headerAlign: "center",
+  {
+    field: "id",
+    headerName: "№",
+    width: 90,
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "customer",
+    headerName: "Клієнт",
+    width: 130,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+    renderCell: params => {
+      const value = params.value as string
+      return <div className={s.ableCell}>{value}</div>
     },
-    {
-      field: "customer",
-      headerName: "Клієнт",
-      width: 130,
-      type: "string",
-      align: "center",
-      headerAlign: "center",
-      renderCell: (params) => {
-        const value = params.value as string;
-        return (
-          <div
-            className={s.ableCell} 
-          >
-            {value}
-          </div>
-        );
-      },
+  },
+  {
+    field: "total",
+    headerName: "Сума",
+    width: 100,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "status",
+    headerName: "Статус",
+    width: 120,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+    renderCell: params => {
+      const status = params.value as string
+      const className = statusColors[status] || s.new_status
+      return <div className={className}>{status}</div>
     },
-    {
-      field: "total",
-      headerName: "Сума",
-      width: 100,
-      type: "string",
-      align: "center",
-      headerAlign: "center",
+  },
+
+  {
+    field: "deliveryMethod",
+    headerName: "Спосіб доставки",
+    width: 140,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "waybill",
+    headerName: "ТТН",
+    width: 140,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "comments",
+    headerName: "Коментар",
+    width: 180,
+    type: "string",
+    align: "center",
+    headerAlign: "center",
+    renderCell: params => {
+      const value = params.value as string
+      return <div className={s.ableCell}>{value}</div>
     },
-    {
-      field: "status",
-      headerName: "Статус",
-      width: 120,
-      type: "string",
-      align: "center",
-      headerAlign: "center",
-      renderCell: (params) => {
-        const status = params.value as string;
-        const className = statusColors[status] || s.new_status;
-        return <div className={className}>{status}</div>;
-      },
-    },
-    
-    {
-      field: "deliveryMethod",
-      headerName: "Спосіб доставки",
-      width: 140,
-      type: "string",     
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-        field: "waybill",
-        headerName: "ТТН",
-        width: 140,
-        type: "string",     
-        align: "center",
-        headerAlign: "center",
-      },
-      {
-        field: "comments",
-        headerName: "Коментар",
-        width: 180,
-        type: "string",     
-        align: "center",
-        headerAlign: "center",
-        renderCell: (params) => {
-          const value = params.value as string;
-          return (
-            <div
-              className={s.ableCell} 
-            >
-              {value}
-            </div>
-          );
-        },
-      },
-    {
-      field: "additionDate",
-      headerName: "Створено",
-      type: "string",
-      width: 102,
-      editable: true,
-      valueGetter: params => moment(params.row.createdAt).format("DD.MM.YYYY"),
-    },
-    
-  ]
-  
+  },
+  {
+    field: "additionDate",
+    headerName: "Створено",
+    type: "string",
+    width: 102,
+    editable: true,
+    valueGetter: params => moment(params.row.createdAt).format("DD.MM.YYYY"),
+  },
+]
